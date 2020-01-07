e3c843349e78d9a09a56a2773bc225937f700dba
miguelamavel <miguel.amavel@gmail.com>
2020-01-07 11:58:53 +0100

    removing la loca scraper -- closed

--- write_menus.py
+++ write_menus.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         KathmanduScraper(),
         TheNestScraper(),
         LemonLeafScraper(),
-        LaLocaScraper(),
+        # LaLocaScraper(),
         EstrellaScraper(),
     ]
 
